e1b02262e45c8524299bdd006e3c20f06d85edf3
charliedoubleu <92770544+charliedoubleu@users.noreply.github.com>
2022-05-31 17:13:43 -0700

    one point OH

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup(){
+    createCanvas(100,100);
+    background(255);
+}
+
+var x = 50;
+var speed = 1;
+function draw(){
+    let a = select('#words');
+    a.position(x,50);
+    x += speed;
+    if(x >= 100){
+        speed = -speed;
+    }
+    if( x <= 1){
+        speed = -speed;
+    }
+}
